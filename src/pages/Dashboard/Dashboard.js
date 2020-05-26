306ea224d1118fdec3c7646f421b7523e25ef4c9
Joao Victor Piga <joaof.victor@hotmail.com>
2020-05-26 17:45:46 -0300

    fix header and lint

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -7,36 +7,36 @@ import Header from '../../assets/components/Header';
 
 
 const Dashboard = () => (
-    <View style={styles.container}>
-        <Header>
-            <View style={{ marginTop: '5%' }}>
-                <Text style={styles.title}>
-                    Bem Vindo Aluno!
-                </Text>
-            </View>
-        </Header>
+  <View style={styles.container}>
+    <Header>
+      <View style={{ marginTop: '5%' }}>
+        <Text style={styles.title}>
+          Bem Vindo Aluno!
+        </Text>
+      </View>
+    </Header>
 
-        <View style={{ flex: 6 }}>
-            <ScrollView>
-                <View style={styles.cardCronograma}>
-                    <View style={styles.cardSubtitle}>
-                        <Text style={styles.cardText}>Cronograma</Text>
-                    </View>
-                    <View style={{ alignSelf: 'stretch' }}>
-                        <Image style={styles.imgCronograma} source={require('../../assets/images/Cronograma.png')}></Image> 
-                    </View>
-                </View>
-
-                <View style={styles.cardAgenda}>
-                    <View style={styles.cardSubtitle}>
-                        <Text style={styles.cardText}>Sua agenda de Estudo</Text>
-                    </View>
-                    <View style={{ alignSelf: 'stretch' }}>
-                        <Image style={styles.imgCronograma} source={require('../../assets/images/agendaEstudo.jpg')}></Image> 
-                    </View>
-                </View>
-            </ScrollView>
+    <View style={{ flex: 6 }}>
+      <ScrollView>
+        <View style={styles.cardCronograma}>
+          <View style={styles.cardSubtitle}>
+            <Text style={styles.cardText}>Cronograma</Text>
+          </View>
+          <View style={{ alignSelf: 'stretch' }}>
+            <Image style={styles.imgCronograma} source={require('../../assets/images/Cronograma.png')} />
+          </View>
         </View>
+
+        <View style={styles.cardAgenda}>
+          <View style={styles.cardSubtitle}>
+            <Text style={styles.cardText}>Sua agenda de Estudo</Text>
+          </View>
+          <View style={{ alignSelf: 'stretch' }}>
+            <Image style={styles.imgCronograma} source={require('../../assets/images/agendaEstudo.jpg')} />
+          </View>
+        </View>
+      </ScrollView>
     </View>
+  </View>
 );
 export default Dashboard;
